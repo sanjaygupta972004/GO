45386e9851d2dd29320296f0fd21cb417b679b53
Sanjay Gupta <sanjaygupta@Sanjays-MacBook-Air.local>
2024-10-20 02:51:50 +0530

    learning channel and deadlock in golang

--- 15channelaurdeadlock/main.go
+++ 15channelaurdeadlock/main.go
@@ -2,9 +2,34 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
 	fmt.Println("channel and deadlock")
 
+	myChan := make(chan int, 3)
+	wg := sync.WaitGroup{}
+
+	wg.Add(2)
+	// RECEIVED ONLY VALUE
+	go func(ch <-chan int, wg *sync.WaitGroup) {
+		val, isChannelOpen := <-myChan
+		if isChannelOpen {
+			fmt.Println(val)
+		}
+		fmt.Println(<-myChan)
+
+		wg.Done()
+	}(myChan, &wg)
+
+	go func(ch chan<- int, wg *sync.WaitGroup) {
+
+		myChan <- 4
+		myChan <- 9
+		close(myChan)
+		wg.Done()
+	}(myChan, &wg)
+
+	wg.Wait()
 }
